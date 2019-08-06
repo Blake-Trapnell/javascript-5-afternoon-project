ad071228b603abe5025c616aed811e571b9df7b3
Blake Trapnell <Blake.Trapnell103@gmail.com>
2019-08-06 15:53:09 -0600

    update

--- constructors.js
+++ constructors.js
@@ -15,6 +15,10 @@
 */
 
 // Code here
+function CarFactory(make, model) {
+  this.make = make
+  this.model = model
+}
 
 ////////// PROBLEM 2 //////////
 
@@ -33,7 +37,7 @@ function Employee(name, email, hireDate) {
   Assign the result of the invocation to a variable called bob.
 */
 
-// Code here
+let bob = new Employee('Bob', 'bob@gmail.com', '01-02-98')
 
 ////////// PROBLEM 3 //////////
 
@@ -56,7 +60,13 @@ mustang.moveCar(); // Increments mustang' move property by 10. Returns the new m
     invoking moveCar on the right object (prius vs mustang).
 */
 
-// Code here
+function Car(make, model, year) {
+    this.make = make
+    this.model = model
+    this.year = year
+    this.move = 0
+    this.moveCar = () => this.move = this.move + 10
+}
 
 ////////// PROBLEM 4 //////////
 
@@ -74,7 +84,7 @@ function Movie(name, genre, rating) {
   this.genre = genre;
   this.rating = rating;
 }
-
+Movie.prototype.changeRating = (num) => this.rating = (this.rating + num) / 2
 // Code here
 
 ////////// PROBLEM 5 //////////
@@ -83,7 +93,15 @@ function Movie(name, genre, rating) {
 
 // Once the User constructor function is created, write a prototype method for the User function. Name this method addSavedPost. It should take in three parameters: id (a number), title (a string) and rating (a number). Use these parameters to create a new object and add it to the savedPosts array. Make sure to name the properties the same as described previously (id, title, rating).
 
-// Code here
+function User (name,age,email, savedPosts) {
+  this.name = name
+  this.age = age
+  this.email = email
+  this.savedPosts = savedPosts
+}
+User.prototype.addSavedPost = (id,title,rating) => {
+  return this.savedPosts.push({id, title, rating})
+}
 
 ////////// PROBLEM 6 //////////
 
